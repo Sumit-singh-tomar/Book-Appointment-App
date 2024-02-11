1ad807eefa88ce905e574a594ded5e8fdd66a4c1
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-11 11:25:30 +0530

    Adding frontend with backend with all functionalities

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-window.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   var ul = document.querySelector('ul')
-  axios.get("https://crudcrud.com/api/ce29ece0a2314de991e22b1cce600613/booking")
+  axios.get("http://localhost:4000/user/get-all-user")
     .then((res) => {
-
+      console.log('resss', res.data.data)
       ul.className = "showDetail"
-      res.data.map((item) => {
+      res.data.data.map((item) => {
         const li = document.createElement('li')
-        li.textContent = `${item.username} - ${item.email} ${item.phone}`
+        li.textContent = `${item.username} - ${item.emailid} ${item.phone}`
         const button = document.createElement('button')
         const editButton = document.createElement('button')
         button.className = "deleteBtn"
         editButton.className = "editBtn"
         button.textContent = "delete"
         editButton.textContent = "Edit"
-        button.id = item._id
-        editButton.id = item._id
-        editButton.name = item.email
+        button.id = item.id
+        editButton.id = item.id
+        editButton.name = item.emailid
         li.appendChild(button)
         li.appendChild(editButton)
         ul.appendChild(li)
@@ -31,16 +31,20 @@ form.addEventListener("submit", function (event) {
   event.preventDefault()
   var userDetail = {
     "username": event.target.username.value,
-    "email": event.target.email.value,
+    "emailid": event.target.email.value,
     "phone": event.target.phone.value
   }
 
-  axios.post("https://crudcrud.com/api/ce29ece0a2314de991e22b1cce600613/booking", userDetail)
-    .then((res) => console.log('success', res))
+  axios.post("http://localhost:4000/user/add-user", userDetail)
+    .then((res) =>{
+      console.log('success', res)
+      location.reload()
+      
+    }) 
     .catch((e) => console.log('error', e))
 
   let email = event.target.email.value
-  localStorage.setItem(email,JSON.stringify(userDetail))
+  localStorage.setItem(email, JSON.stringify(userDetail))
 
   // const ul = document.querySelector('ul')
   // ul.className = "showDetail"
@@ -74,37 +78,33 @@ const showDetail = document.querySelector('ul');
 showDetail.addEventListener('click', function (event) {
 
   if (event.target.classList.contains("deleteBtn")) {
-    let elementToRemove = event.target.parentElement;
-    showDetail.removeChild(elementToRemove);
-    
-    axios.delete(`https://crudcrud.com/api/ce29ece0a2314de991e22b1cce600613/booking/${event.target.id}`)
-      .then((res)=>{
-        alert('user delete successfully')
-      }).catch((e)=>{
-        alert('failed')
-        console.log('eeeeeeeeee',e)
-      })
 
+    axios.delete(`http://localhost:4000/user/delete-user/${event.target.id}`)
+      .then((res) => {
+        let elementToRemove = event.target.parentElement;
+        showDetail.removeChild(elementToRemove);
+        alert('user delete successfully')
+      }).catch((e) => {
+        alert('failed')
+      })
   }
 
   if (event.target.classList.contains("editBtn")) {
     let elementToRemove = event.target.parentElement;
-    console.log(elementToRemove)
     var userData = JSON.parse(localStorage.getItem(event.target.name))
     const username = document.getElementById('username')
     username.value = userData.username
-    email.value = userData.email
+    email.value = userData.emailid
     phone.value = userData.phone
-    console.log('userdata',userData)
 
     showDetail.removeChild(elementToRemove);
     localStorage.removeItem(userData.email);
 
-    axios.delete(`https://crudcrud.com/api/ce29ece0a2314de991e22b1cce600613/booking/${event.target.id}`)
-      .then((res)=>{
-      console.log('user updated successfully')
-    }).catch((e)=>{
-      console.log('eeeeeeeeee',e)
-    })
+    axios.delete(`http://localhost:4000/user/delete-user/${event.target.id}`)
+      .then((res) => {
+        console.log('user updated successfully')
+      }).catch((e) => {
+        console.log('eeeeeeeeee', e)
+      })
   }
 });
